0ca0a560cb018e799e420fc4f5920c9b8f0dd643
Matheus Souza dos santos <msdsantos96@gmail.com>
2025-04-08 00:07:52 -0300

    fix: getbyCPF

--- pipeline/getByCPF.py
+++ pipeline/getByCPF.py
@@ -29,36 +29,37 @@ def get_lattes_id(cpf, nome, data_nascimento=None):
         print(f"Erro ao buscar ID para {nome}: {e}")
         return None
 
-df = pd.read_excel('MPUBA25.03.2025.xlsx')
+df = pd.read_excel('MPU (1).xlsx')
 
-with engine.begin() as conn:  
+with engine.connect() as conn:
+    trans = conn.begin() 
 
-    institution_query = select(institution_table)
-    institution_result = conn.execute(institution_query).mappings().all()
+    try:
+        institution_query = select(institution_table)
+        institution_result = conn.execute(institution_query).mappings().all()
 
-    institution_map = {
-        row['acronym'].strip().lower(): row['institution_id'] for row in institution_result
-    }
+        institution_map = {
+            row['acronym'].strip().lower(): row['institution_id'] for row in institution_result
+        }
 
-    # Loop pelos dados
-    for _, row in df.iterrows():
-        try:
-            name = row['NOME EMPR./CANDIDATO']
-            cpf = row['NÚMERO DO CPF'].replace('.', '').replace('-', '')
-            institution_acronym = row['EMPRESA'].strip().lower()
+        # Loop pelos dados do DataFrame
+        for _, row in df.iterrows():
+            try:
+                name = row['NOME EMPR./CANDIDATO']
+                cpf = row['NÚMERO DO CPF'].replace('.', '').replace('-', '')
+                institution_acronym = row['EMPRESA'].strip().lower()
 
-            id_lattes = get_lattes_id(cpf, name)
-            print(f"Extraído: {id_lattes}")
-        except Exception as e:
-            print("Erro ao extrair dados:", e)
-            continue
+                id_lattes = get_lattes_id(cpf, name)
+                print(f"Extraído: {id_lattes}")
+            except Exception as e:
+                print("Erro ao extrair dados:", e)
+                continue
 
-        id_institution = institution_map.get(institution_acronym)
+            id_institution = institution_map.get(institution_acronym)
 
-        if id_institution and isinstance(id_lattes, int):
+            # Verifica se já existe no banco
             query = select(researcher_table).where(researcher_table.c.lattes_id == str(id_lattes))
             result = conn.execute(query).fetchone()
-            print(result)
 
             if not result:
                 insert_stmt = researcher_table.insert().values(
@@ -70,5 +71,9 @@ with engine.begin() as conn:
                 print(f"Inserido: {name.title()}")
             else:
                 print(f"Já existe no banco: {name.title()}")
-        else:
-            print(f"Dados inválidos para {name}: id_institution={id_institution}, id_lattes={id_lattes}")
+
+        trans.commit()  
+
+    except Exception as e:
+        print("Erro durante a transação:", e)
+        trans.rollback()  
