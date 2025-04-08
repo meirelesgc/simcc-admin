f1264c3e1515361e999901c6115d995ac0bb4e36
Matheus Souza dos santos <msdsantos96@gmail.com>
2025-04-08 01:35:53 -0300

    feat: add strip

--- pipeline/getByCPF.py
+++ pipeline/getByCPF.py
@@ -52,7 +52,7 @@ with engine.connect() as conn:
 
         for _, row in df.iterrows():
             name = row['NOME EMPR./CANDIDATO']
-            cpf = row['NÚMERO DO CPF'].replace('.', '').replace('-', '')
+            cpf = str(row['NÚMERO DO CPF']).replace('.', '').replace('-', '').strip()
             institution_acronym = row['EMPRESA'].strip().lower()
 
             id_lattes = get_lattes_id(cpf, name)
