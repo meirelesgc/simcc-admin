753f775f2022bebcd5ff77d307ba5cd8709dd985
Matheus Souza dos santos <msdsantos96@gmail.com>
2025-04-07 23:41:39 -0300

    feat: add find by institution

--- pipeline/getByCPF.py
+++ pipeline/getByCPF.py
@@ -2,30 +2,22 @@ import pandas as pd
 from zeep import Client
 from sqlalchemy import create_engine, select, Table, MetaData
 
-
 DB_USER = 'postgres'
 DB_PASSWORD = 'postgres'
 DB_HOST = 'localhost'  
 DB_PORT = '5432'
-DB_NAME = 'simcc'
+DB_NAME = 'simcc_admin'
 
 engine = create_engine(f'postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}')
-
 metadata = MetaData()
 metadata.reflect(bind=engine)
-researcher_table = metadata.tables['researcher']
 
-instituition = [
-    {'name': 'UEFS', 'id': '083a16f0-cccf-47d2-a676-d10b8931f66b'},
-    {'name': 'Universidade Do Estado Da Bahia', 'id': '3215039c-57a3-4325-a578-ccf001a83864'},
-    {'name': 'Universidade Federal Da Bahia', 'id': '34baec39-a219-4d8f-9c8e-40a158bffa00'},
-]
+researcher_table = metadata.tables['researcher']
+institution_table = metadata.tables['institution']
 
 # Função para obter o ID Lattes via CPF
 def get_lattes_id(cpf, nome, data_nascimento=None):
-
     client = Client("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl")
-
     try:
         result = client.service.getIdentificadorCNPq(
             cpf=cpf,
@@ -37,18 +29,23 @@ def get_lattes_id(cpf, nome, data_nascimento=None):
         print(f"Erro ao buscar ID para {nome}: {e}")
         return None
 
-
 df = pd.read_excel('MPUBA25.03.2025.xlsx')
 
-# Loop pelos dados
-with engine.connect() as conn:
+with engine.begin() as conn:  
+
+    institution_query = select(institution_table)
+    institution_result = conn.execute(institution_query).mappings().all()
+
+    institution_map = {
+        row['acronym'].strip().lower(): row['institution_id'] for row in institution_result
+    }
+
+    # Loop pelos dados
     for _, row in df.iterrows():
         try:
             name = row['NOME EMPR./CANDIDATO']
             cpf = row['NÚMERO DO CPF'].replace('.', '').replace('-', '')
-            instituition_name = row['EMPRESA']
-
-            instituition_info = next((e for e in instituition if e['name'].strip().lower() == instituition_name.strip().lower()), None)
+            institution_acronym = row['EMPRESA'].strip().lower()
 
             id_lattes = get_lattes_id(cpf, name)
             print(f"Extraído: {id_lattes}")
@@ -56,17 +53,17 @@ with engine.connect() as conn:
             print("Erro ao extrair dados:", e)
             continue
 
-        # Continua apenas se empresa_info existir e id_lattes for inteiro
-        if instituition_info and isinstance(id_lattes, int):
-            id_instituition= instituition_info['id']
+        id_institution = institution_map.get(institution_acronym)
 
+        if id_institution and isinstance(id_lattes, int):
             query = select(researcher_table).where(researcher_table.c.lattes_id == str(id_lattes))
             result = conn.execute(query).fetchone()
             print(result)
+
             if not result:
                 insert_stmt = researcher_table.insert().values(
                     name=name.title(),
-                    institution_id=id_instituition,
+                    institution_id=id_institution,
                     lattes_id=id_lattes
                 )
                 conn.execute(insert_stmt)
@@ -74,5 +71,4 @@ with engine.connect() as conn:
             else:
                 print(f"Já existe no banco: {name.title()}")
         else:
-            print(f"Dados inválidos para {name}: empresa_info={instituition_info}, id_lattes={id_lattes}")
-
+            print(f"Dados inválidos para {name}: id_institution={id_institution}, id_lattes={id_lattes}")
