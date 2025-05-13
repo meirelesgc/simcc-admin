1bdad1dec7d8137b74d629c1cf915b6b040fd993
gmcosta <geu_costa@outlook.com>
2025-05-13 18:39:42 -0300

    dev: adicionando programas de pós

--- scripts/xpto.py
+++ scripts/xpto.py
@@ -1,17 +1,50 @@
+import ast
+import unicodedata
+
 import pandas as pd
-from zeep import Client
 
-columns = ["aconym", "name", "CPF"]
-client = Client("http://servicosweb.cnpq.br/srvcurriculo/WSCurriculo?wsdl")
-dataframe = pd.read_csv("pesquisadores.CSV", sep=";")
+from adm_simcc.dao import Connection
 
-for _, data in dataframe.iterrows():
-    try:
-        cpf = data.get("cpf").replace(".", "").replace("-", "")
-        response = client.service.getIdentificadorCNPq(
-            cpf=cpf, nomeCompleto="", dataNascimento=""
-        )
-        if response:
-            data.lattes_id = response
-    except Exception:
-        print(f"Erro identificado no registro {_}")
+
+def normalize_keys(d):
+    def normalize(s):
+        s = unicodedata.normalize("NFD", s)
+        s = "".join(c for c in s if unicodedata.category(c) != "Mn")
+        return s.lower()
+
+    return {normalize(k): v for k, v in d.items()}
+
+
+programs = pd.read_csv("files/programs.csv")
+print(programs.columns)
+error = 0
+for _, program in programs.iterrows():
+    pg = program.to_dict()
+    if pg["situação"] == "EM FUNCIONAMENTO":
+        try:
+            print("\n\n", f"[{_}] - ID")
+            _type = list()
+            for course in ast.literal_eval(pg["cursos"]):
+                _course = normalize_keys(course)
+                if _course["situacao"] == "EM FUNCIONAMENTO":
+                    _type.append(_course["nivel"])
+                print(_course["nota"])
+                SCRIPT_SQL = """
+                    INSERT INTO public.graduate_program(
+                        code, name, area, modality, type, rating,
+                        institution_id, state, city, region, url_image, acronym,
+                        description, visible, site, created_at, updated_at)
+                    VALUES (%(código)s, %(nome)s, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+                    """
+                SCRIPT_SQL = """
+                    SELECT (%(código)s, %(nome)s, %(área de avaliação)s, '',
+                        %(type)s)
+                    """
+            pg["type"] = "/".join(_type)
+            # print(Connection().select(SCRIPT_SQL, pg))
+        except Exception:
+            error += 1
+    else:
+        error += 1
+
+print(error)
