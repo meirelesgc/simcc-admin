0c20023d8c0c91586f4a62c918e5ea695360b5ae
gmcosta <geu_costa@outlook.com>
2025-07-08 18:28:10 -0300

    wip

--- adm_simcc/models/graduate_program.py
+++ adm_simcc/models/graduate_program.py
@@ -4,30 +4,30 @@ from pydantic import UUID4, BaseModel
 
 
 class GraduateProgram(BaseModel):
-    graduate_program_id: UUID4
-    code: Optional[str]
-    name: str
-    name_en: Optional[str]
-    basic_area: Optional[str]
-    cooperation_project: Optional[str]
-    area: str
-    modality: str
-    program_type: Optional[str]
-    rating: Optional[str]
+    graduate_program_id: UUID4 = None
+    code: Optional[str] = None
+    name: str = None
+    name_en: Optional[str] = None
+    basic_area: Optional[str] = None
+    cooperation_project: Optional[str] = None
+    area: str = None
+    modality: str = None
+    program_type: Optional[str] = None
+    rating: Optional[str] = None
     institution_id: UUID4
     state: str = "BA"
     city: str = "Salvador"
     region: str = "Nordeste"
     url_image: Optional[str] = None
-    acronym: Optional[str]
-    description: Optional[str]
+    acronym: Optional[str] = None
+    description: Optional[str] = None
     visible: bool = False
     site: Optional[str] = None
-    coordinator: Optional[str]
-    email: Optional[str]
-    start: Optional[Any]
-    phone: Optional[str]
-    periodicity: Optional[str]
+    coordinator: Optional[str] = None
+    email: Optional[str] = None
+    start: Optional[Any] = None
+    phone: Optional[str] = None
+    periodicity: Optional[str] = None
 
 
 class ListGraduateProgram(BaseModel):
