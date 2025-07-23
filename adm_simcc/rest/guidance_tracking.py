bd5ffe6f560ea507d310f11aac35ade2878f958d
gmcosta <geu_costa@outlook.com>
2025-07-23 11:21:12 -0300

    wip

--- adm_simcc/rest/guidance_tracking.py
+++ adm_simcc/rest/guidance_tracking.py
@@ -11,26 +11,26 @@ from ..dao.dao_guidnace_tracking import (
 guidance_tracking_bp = Blueprint("guidance_tracking", __name__)
 
 
-@guidance_tracking_bp.route("/guidance_tracking", methods=["GET"])
+@guidance_tracking_bp.route("/guidance_tracking/", methods=["GET"])
 def get_all():
     return get_all_guidance_trackings()
 
 
 @guidance_tracking_bp.route(
-    "/guidance_tracking/<uuid:guidance_id>", methods=["GET"]
+    "/guidance_tracking/<uuid:guidance_id>/", methods=["GET"]
 )
 def get_one(guidance_id):
     return get_guidance_tracking_by_id(guidance_id)
 
 
-@guidance_tracking_bp.route("/guidance_tracking", methods=["POST"])
+@guidance_tracking_bp.route("/guidance_tracking/", methods=["POST"])
 def create():
     data = request.get_json()
     return create_guidance_tracking(data)
 
 
 @guidance_tracking_bp.route(
-    "/guidance_tracking/<uuid:guidance_id>", methods=["PUT"]
+    "/guidance_tracking/<uuid:guidance_id>/", methods=["PUT"]
 )
 def update(guidance_id):
     data = request.get_json()
@@ -38,7 +38,7 @@ def update(guidance_id):
 
 
 @guidance_tracking_bp.route(
-    "/guidance_tracking/<uuid:guidance_id>", methods=["DELETE"]
+    "/guidance_tracking/<uuid:guidance_id>/", methods=["DELETE"]
 )
 def delete(guidance_id):
     return delete_guidance_tracking(guidance_id)
